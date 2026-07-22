77fb7b155c08c999cfe64c49c76cf42b095d46c1
Juan Diego Silva Sotelo <juan.sotelo@ambipar.com>
2026-07-22 15:10:44 -0500

    Pipeline ETL de auditoría finalizado y generando JSON

--- auditoria.py
+++ auditoria.py
@@ -1,17 +1,22 @@
-from datos import lote_inspecciones
+import json
+
+with open('inspecciones.json', 'r') as archivo:
+    lote_inspecciones = json.load(archivo)
+
 total_alertas = 0
 
-
+lista_errores = []
 
 for inspeccion in lote_inspecciones:
 
     if inspeccion['estado'] == "Rechazado":
-
         total_alertas += 1
-
         print(f"¡ALERTA! El puesto con reporte {inspeccion['id_reporte']} falló la inspección.")
+        lista_errores.append(inspeccion)
 
 
+with open ('errores_detectados.json', 'w') as archivo:
+    json.dump(lista_errores,archivo)
 
-print(f"Auditoría finalizada. Total de alertas generadas: {total_alertas}") 
+print(f"Auditoría finalizada. Total de alertas generadas: {total_alertas}")
 
